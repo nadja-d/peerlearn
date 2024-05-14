42124b678310cd2adbc0da544f2dda417da83612
Nadja <nadja.donosepoetro@gmail.com>
2024-05-15 00:42:24 +0700

    added the pictures

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 
-Route::get('/', function () {
-    return view('register');
-});
+Route::get('/', function () { return view('register');});
 
 Route::post('/register', [userController::class, 'createUser'])->name('register');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login'); 
+Route::get('/login', function () {return view('login');})->name('login'); 
 
